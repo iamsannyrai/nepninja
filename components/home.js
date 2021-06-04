aec6ed9a5774321088fac5e55824184a5fd4edc0
Sanny Rai <iam.sannyrai@gmail.com>
2021-06-04 21:27:54 +0545

    creates space to inject blog

--- components/home.js
+++ components/home.js
@@ -1,9 +1,12 @@
+import { BlogList } from "./blog_list";
 import { LatestBlog } from "./latest";
 
-export function HomeContent({ blog }) {
+export function HomeContent({ blog, blogList }) {
     return <div>
         <div className="my-6 px-4 md:px-10 md:flex md:space-x-4">
             <LatestBlog blog={blog} />
+            <div className="w-4"></div>
+            <BlogList blogList={blogList} />
         </div>
         <hr />
     </div >
--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,5 @@ import AppLayout from '../components/layout'
 import { HomeContent } from '../components/home'
 
 export default function Home() {
-  return <AppLayout child={<HomeContent />} />
+  return <AppLayout child={<HomeContent blog={ } blogList={ } />} />
 }
