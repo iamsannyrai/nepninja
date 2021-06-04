e4406b9b3ce840712f894c8a747ebfcd2580b049
Sanny Rai <iam.sannyrai@gmail.com>
2021-06-04 14:44:03 +0545

    creates footer

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import { Footer } from "../components/footer";
 import { Navbar } from "../components/navbar";
 
 export default function Home() {
   return <div>
     <Navbar />
+    <Footer />
   </div>
 }
