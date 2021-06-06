8f6d730d9628a63e4b64362e8fb4b537cef4879b
Sanny Rai <iam.sannyrai@gmail.com>
2021-06-06 10:05:10 +0545

    install tailwind typography and used prism syntax highlighter

--- components/layout.js
+++ components/layout.js
@@ -7,6 +7,9 @@ export default function AppLayout({ child }) {
     return <>
         <Head>
             <title>NepNinja</title>
+            <link
+                href={`https://unpkg.com/prismjs@0.0.1/themes/prism-tomorrow.css`}
+                rel="stylesheet" />
             <meta name="description" content="NepNinja Blog" />
         </Head>
         <Navbar />
