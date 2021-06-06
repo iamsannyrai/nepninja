7adc3173052050f168d32c864106e74274842ebe
Sanny Rai <iam.sannyrai@gmail.com>
2021-06-06 10:00:54 +0545

    builds detail page by parsing markdown file

--- bloc/blog.js
+++ bloc/blog.js
@@ -1,6 +1,9 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+import remark from 'remark'
+import html from 'remark-html'
+import prism from 'remark-prism'
 
 const allContentPath = path.join(process.cwd(), 'contents')
 
@@ -36,4 +39,55 @@ export function getSortedBlogList() {
             return -1
         }
     })
+}
+
+
+export function getAllPostIds() {
+    const folders = fs.readdirSync(allContentPath)
+    const allMarkdownFiles = [];
+
+    folders.forEach(folder => {
+        const blogDirectory = path.join(allContentPath, folder)
+        const fileName = fs.readdirSync(blogDirectory)
+
+        const markdownFile = fileName.find((file) => file.endsWith(".md"))
+
+        allMarkdownFiles.push(markdownFile)
+    })
+
+    return allMarkdownFiles.map(fileName => {
+        return {
+            params: {
+                id: fileName.replace(/\.md$/, '')
+            }
+        }
+    })
+}
+
+
+export async function getPostData(id) {
+    const folders = fs.readdirSync(allContentPath)
+
+    const targetFolder = folders.find((folder) => folder == id)
+
+    const blogDirectory = path.join(allContentPath, targetFolder)
+    const fullPath = path.join(blogDirectory, `${id}.md`)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents)
+
+    // Use remark to convert markdown into HTML string
+    const processedContent = await remark()
+        .use(html).use(prism)
+        .process(matterResult.content)
+
+    const contentHtml = processedContent.toString()
+
+    // Combine the data with the id
+    return {
+        id,
+        contentHtml,
+        ...matterResult.data
+    }
 }
