e9165d2f4c8e10b7a04fb37da62fd868e99f1b18
Sanny Rai <iam.sannyrai@gmail.com>
2021-06-06 10:08:28 +0545

    links to detail page from blog title

--- components/latest.js
+++ components/latest.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export function LatestBlog({ blog }) {
     return <div className="my-1">
-        <Link href="/"><a className="text-3xl font-bold font-serif hover:text-blue-600">{blog.title}</a></Link>
+        <Link href={`/blogs/${blog.id}`}><a className="text-3xl font-bold font-serif hover:text-blue-600">{blog.title}</a></Link>
         <div className="my-4">
             <p>{blog.subtitle}</p>
         </div>
