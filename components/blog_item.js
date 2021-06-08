28f93d2cec464664d9ffa7823a7fa5b0cad2680a
Sanny Rai <iam.sannyrai@gmail.com>
2021-06-08 18:42:40 +0545

    few fixes

--- components/blog_item.js
+++ components/blog_item.js
@@ -2,18 +2,16 @@ import Link from "next/link"
 import Image from "next/image"
 import ReadableDate from "./utility/date_utility";
 
-export function BlogItem({ blog }) {
-    return <div>
-        <div className="flex mb-4 h-100">
-            <div className="flex-shrink-0 mr-6">
-                <Image src={blog.image} height={80} width={80} className="rounded-md" />
-            </div>
-            <div>
-                <Link href={`/blogs/${blog.id}`}><a className="font-semibold text-lg font-serif hover:text-blue-500">{blog.title}</a></Link>
-                <p className="text-gray-700">{blog.subtitle}</p>
-                <span className="text-sm font-light mt-4"><ReadableDate dateString={blog.date} /></span>
-            </div>
+export function BlogItem({ blog, isLast }) {
+    return <div className="flex mb-4 h-100 border rounded-lg px-4 py-2">
+        <div className="flex-shrink-0 mr-6 mt-2">
+            <Image src={blog.image} height={80} width={80} className="rounded" />
         </div>
-        <hr className="mb-2" />
+        <div>
+            <Link href={`/blogs/${blog.id}`}><a className="font-semibold text-lg font-serif hover:text-blue-500">{blog.title}</a></Link>
+            <p className="text-gray-700">{blog.subtitle}</p>
+            <span className="text-sm font-light mt-4"><ReadableDate dateString={blog.date} /></span>
+        </div>
     </div>
+
 }
--- components/blog_list.js
+++ components/blog_list.js
@@ -5,8 +5,5 @@ export function BlogList({ blogList }) {
         {blogList.map((blog) => (
             <BlogItem key={blog.title} blog={blog} />
         ))}
-        <div className="text-right mt-10">
-            <button className="bg-blue-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-blue-500">Explore All</button>
-        </div>
     </div>
 }
